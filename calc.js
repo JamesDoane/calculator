40b94deec757514b9653077f79116cd6ab032b73
JamesDoane <ijamesdoaneok@gmail.com>
2021-07-26 15:19:00 -0600

    Addition, Subtraction, Multiplication, Division, and square root functions

--- calc.js
+++ calc.js
@@ -13,6 +13,35 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	if(tokens[0] === '+'){
+		let sum = num1 + num2;
+		console.log(num1, " + ", num2, " = ", sum);
+		reader.close;
+	}
+	else if(tokens[0] === '-'){
+		let sum = num1 - num2;
+		console.log(num1, " - ", num2, " = ", sum);
+		reader.close;
+	}
+	else if(tokens[0] === '*'){
+		let sum = num1 * num2;
+		console.log(num1, " * ", num2, " = ", sum);
+		reader.close;
+	}
+	else if(tokens[0] === '/'){
+	let sum = num1 / num2;
+	console.log(num1, " / ", num2, " = ", sum);
+	reader.close;
+	}
+	else if(tokens[0] === "sqrt"){
+		let sqrt = Math.sqrt(num1);
+		console.log("Square root of", num1, "is " , sqrt);
+		reader.close;
+	}
+	else{
+		onsole.log("Invalid Entry");
+	}
+
 
 
 	// This line closes the connection to the command line interface.
